16cd598338dea52b6db5070dc8c0f2420faae574
glory2win <svgoals69@gmail.com>
2024-06-03 12:35:12 +0300

    Added Category title fonts, text and setting and menu buttons - Added needed resources. Learn how to fetch custom font from resources intead of using one from user's machine.

--- TaskFlow.cpp
+++ TaskFlow.cpp
@@ -1,13 +1,52 @@
 #include "TaskFlow.h"
+
 #include <QPixmap>
+#include <QIcon>
+#include <QFontDatabase>
 
 TaskFlow::TaskFlow(QWidget *parent)
     : QWidget(parent)
 {
     ui.setupUi(this);
 
-    QPixmap titlePixMap(":/images/TaskFlowAppTitle.png");
+    const QPixmap titlePixMap(":/images/TaskFlowAppTitle.png");
     ui.appTitleLabel->setPixmap(titlePixMap);
+
+    const QPixmap settingsIconPixmap(":/images/settings.png");
+    const QIcon settingsIcon(settingsIconPixmap);
+    ui.settingsBtn->setIcon(settingsIcon);
+    ui.settingsBtn->setIconSize(QSize(32, 32));
+    ui.settingsBtn->setText("");
+
+    const QPixmap listOptionsPixmap(":/images/menu.png");
+    const QIcon listOptionsIcon(listOptionsPixmap);
+    ui.listOptionsBtn->setIcon(listOptionsIcon);
+    ui.listOptionsBtn->setIconSize(QSize(32, 32));
+    ui.listOptionsBtn->setText("");
+
+    // IMP: When setting up the font with "Arial" kind of notation in QFont will query the font from the computer not from resources
+    // In case of custom font, we have to load that first and then create QFont from that. To do that use QFontDatabase class
+    if (const int fontId = QFontDatabase::addApplicationFont(":/fonts/Itim-Regular.ttf"); fontId != -1)
+    // -1 mean no failure to load the font
+    {
+	    QStringList fontFamilies = QFontDatabase::applicationFontFamilies(fontId);
+	    if (!fontFamilies.empty())
+	    {
+		    const QString& fontName = fontFamilies.at(0); // Get the first one.
+		    constexpr int fontSize = 28;
+		    QFont listFont(fontName, fontSize);
+            ui.categoryTitleLabel->setFont(listFont);
+            ui.categoryTitleLabel->setText("Groceries");
+	    }
+	    else
+	    {
+		    qWarning("[Font] - Font family is empty");
+	    }
+    }
+    else
+    {
+        qWarning("Unable to get the font!");
+    }
 
 }
 
